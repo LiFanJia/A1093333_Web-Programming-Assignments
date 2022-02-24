d1bb01c335e0cccbd29d9ca564533ad60f2aa3d5
LiFanJia <100353125+LiFanJia@users.noreply.github.com>
2022-02-24 23:01:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+ <head>
+
+</head>
+
+<body>
+
+<?php
+
+$name="PHP";
+$version=8;
+echo"<h1> <b> HELLO! $name  $version </b> </h1>";
+
+echo "<br/>";
+
+echo date("Y/m/d");
+
+
+
+
+?>
+</body>
+
+</html>
+
